4bb90a3e26478f5374d591995adeee7ccd499c01
Josielly Tomas da Fonseca <139061780+JosiellyTomas@users.noreply.github.com>
2023-08-20 15:44:50 -0300

    Update main.dart

--- ecommerce/lib/main.dart
+++ ecommerce/lib/main.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:teste01/Footer.dart';
+
+
 
 
 void main() {
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+   return const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Footer(),
+   );
+  }
+
+  
 }
